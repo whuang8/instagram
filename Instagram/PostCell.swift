4bba841693d480599ff7d44c193abf264047f270
whuang8 <huang554@purdue.edu>
2017-03-21 01:32:02 -0400

    Design post cell

--- Instagram/PostCell.swift
+++ Instagram/PostCell.swift
@@ -12,16 +12,26 @@ import ParseUI
 
 class PostCell: UITableViewCell {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentUsernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var postImageView: PFImageView!
     var post: Post! {
         didSet {
             self.postImageView.file = post.media
             self.postImageView.loadInBackground()
+            self.usernameLabel.text = post.author.username
+            self.commentUsernameLabel.text = post.author.username
+            self.captionLabel.text = post.caption
+            self.profileImageView.image = UIImage(named: "insta-colors")
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.profileImageView.layer.cornerRadius = self.profileImageView.bounds.width/2
+        self.profileImageView.clipsToBounds = true
         self.selectionStyle = .none
     }
 
